292dd3f14ae20f3b02f4edfe94ff7ff33f7194e7
ask <perekatipole@gmail.com>
2025-09-27 14:25:27 +0500

    tasks ##1-5

--- apps/smart_home/init.sql
+++ apps/smart_home/init.sql
@@ -1,5 +1,9 @@
--- Create the database if it doesn't exist
+-- Create the database if it doesn't exist (PostgreSQL 12+)
+-- Note: IF NOT EXISTS is not supported for CREATE DATABASE in PostgreSQL
+-- Use this workaround with psql meta-commands
+\set ON_ERROR_STOP off
 CREATE DATABASE smarthome;
+\set ON_ERROR_STOP on
 
 -- Connect to the database
 \c smarthome;
